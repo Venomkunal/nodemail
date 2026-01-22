77a8f9eaa526c8cb31220300c2cc155d816012ab
Venomkunal <104896882+Venomkunal@users.noreply.github.com>
2026-01-22 22:01:17 +0530

    Replace header with head in RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,13 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <header>
+        <head>
           {/* <!-- Canonical --> */}
     <link rel="canonical" href="https://www.shinewebtechcretions.online/" />
 
     {/* <!-- Favicon --> */}
     <link rel="icon" type="image/svg+xml" href="https://www.shinewebtechcretions.online/favicon.ico" />
-        </header>
+        </head>
       <body>{children}</body>
     </html>
   );
