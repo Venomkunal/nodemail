66893cf44d55d6f136d3b8be302531833d9e1cf0
Venomkunal <104896882+Venomkunal@users.noreply.github.com>
2026-01-22 21:59:31 +0530

    Add header with canonical link and favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,13 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <header>
+          {/* <!-- Canonical --> */}
+    <link rel="canonical" href="https://www.shinewebtechcretions.online/" />
+
+    {/* <!-- Favicon --> */}
+    <link rel="icon" type="image/svg+xml" href="https://www.shinewebtechcretions.online/favicon.ico" />
+        </header>
       <body>{children}</body>
     </html>
   );
